bffd87e5a150c5d5e9c96259d56a36075eadfcd1
Arup3201 <arupjana7365@gmail.com>
2025-08-14 17:20:30 +0530

    update: added binary search

--- array/array.go
+++ array/array.go
@@ -9,3 +9,20 @@ func Search1DArray(arr []int, x int) int {
 
 	return -1
 }
+
+func BinarySearch(arr []int, x int) int {
+	var low, high int = 0, len(arr)
+
+	for low <= high {
+		mid := low + (high-low)/2
+		if x == arr[mid] {
+			return mid
+		} else if x > arr[mid] {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return -1
+}
--- main.go
+++ main.go
@@ -6,8 +6,11 @@ import (
 )
 
 func main() {
-	// code to execute module functions
 	a := []int{2, 4, 1, 6, 10, 8}
 	index := array.Search1DArray(a, 10)
-	fmt.Printf("Element position: %d\n", index)
+	fmt.Printf("Linear search in the array %v: %d\n", a, index)
+
+	b := []int{2, 4, 6, 9, 10, 24, 30}
+	index = array.BinarySearch(b, 10)
+	fmt.Printf("Binary search in the array %v: %d\n", b, index)
 }
