45aadb04cd35354b7da2db8e46d09e62b3d93541
Arup3201 <arupjana7365@gmail.com>
2025-08-16 11:11:55 +0530

    update: stdin program added

---
+++ dup1/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	for text, c := range counts {
+		if c > 0 {
+			fmt.Printf("%d\t%s\n", c, text)
+		}
+	}
+}
