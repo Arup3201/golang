a2124fba3f25b45eecc6c3fb5f1c73a1ce9880bb
Arup3201 <arupjana7365@gmail.com>
2025-08-18 22:13:53 +0530

    update: string example and tests added

---
+++ string/string_test.go
@@ -0,0 +1,41 @@
+package string
+
+import (
+	"testing"
+)
+
+func TestStringLen(t *testing.T) {
+	str := "Hello, world"
+	want := 12
+	if got := len(str); got != want {
+		t.Errorf("len(str)=%d, want %d", got, want)
+	}
+}
+
+func TestStringInd(t *testing.T) {
+	str := "Hello, world"
+	want := byte(',')
+
+	if got := str[5]; got != want {
+		t.Errorf("str[5]=%d, want %d", got, want)
+	}
+}
+
+func TestRawString(t *testing.T) {
+	const GoUsage = `Go is a tool for managing Go source code.
+	
+	Usage: 
+		go command [arguments]
+		...`
+
+	t.Log(GoUsage)
+}
+
+func TestUTF8Rune(t *testing.T) {
+	utf8Rune := '世'
+	want := 0x4e16
+
+	if int32(utf8Rune) != int32(want) {
+		t.Errorf("expected %x, got %x", want, utf8Rune)
+	}
+}
